f4675f2c5bbc9a856760699a4bfa604404f82dc0
Andrea T <tostonar@gmail.com>
2019-08-31 21:01:00 -0400

    All works except investment method

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,24 @@
 class FundingRound
+  attr_reader :startup, :venture_capitalist, :investment
+  attr_accessor :type
 
+  @@all = []
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment.to_f
+    @@all << self
+  end
+
+  #TODO how to block contract if investment not float or positive?
+  def investment=(investment)
+    if investment.positive? && investment.class == Float
+      @investment = investment
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,46 @@
 class VentureCapitalist
+  attr_reader :name
+  attr_accessor :total_worth
+
+  @@all = []
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth.to_f
+    @@all << self
+  end
+
+  def offer_contract(startup, type, investment)
+    contract = FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|funding_round| funding_round.venture_capitalist == self}  
+  end
+
+  def portfolio
+    funding_rounds.map {|funding_round| funding_round.startup}.uniq
+  end
+
+  def biggest_investment
+    funding_rounds.max_by {|f| f.investment}
+  end
+
+  def startups
+    portfolio.map {|f| f.startup}.map {|s| s.domain}
+  end
+
+  
+  def invested(domain)
+    #from all funding_rounds, get all where startup.domain matches given domain, then get investment, then sum
+    funding_rounds.select {|f| f.startup.domain == domain}.map {|f| f.investment}.sum
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    big_bucks = @@all.select {|x| x.total_worth > 1000000 }
+  end
 
 end
