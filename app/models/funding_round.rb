7f84577ef715509f59d4b87da919ef4f4c957b0d
Mark Del Mar <mark@1000martians.com>
2019-09-02 11:27:29 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,19 @@
 class FundingRound
 
+  attr_accessor :startup, :venture_capitalist, :type, :investment
+  @@all = []
+
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment.to_f.round(2)
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+apple = Startup.new("Apple", "Steve Jobs", "apple.com")
+google = Startup.new("Google", "Matt Damon", "google.com")
+kickstarter = Startup.new("Kickstarter", "Ben Affleck", "kickstarter.com")
+
+mark = VentureCapitalist.new("Mark", 563570)
+alex = VentureCapitalist.new("Alex", 1000)
+tim = VentureCapitalist.new("Tim", 999999999999)
+
+round1 = FundingRound.new(apple, mark, "type", 1000)
+round2 = FundingRound.new(google, alex, "type", 5000.134)
+round3 = FundingRound.new(apple, tim, "type", 235643.654)
+round4 = FundingRound.new(apple, mark, "type", 696969)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
